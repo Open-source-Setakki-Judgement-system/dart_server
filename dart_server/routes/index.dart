e4c4b197900317812057b4dcd8aec78c3e2e9412
Yoochanhong <chanhongy6@naver.com>
2022-12-04 21:16:53 +0900

    더미 데이터용 서버 생성

--- dart_server/routes/index.dart
+++ dart_server/routes/index.dart
@@ -1,5 +1,26 @@
+import 'dart:convert';
+
 import 'package:dart_frog/dart_frog.dart';
 
+var jsonData = [
+  {
+    "id": 1,
+    "state": 1
+  },
+  {
+    "id": 2,
+    "state": 0
+  },
+  {
+    "id": 3,
+    "state": 0
+  },
+  {
+    "id": 4,
+    "state": 1
+  }
+];
+
 Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+  return Response(body: jsonEncode(jsonData));
 }

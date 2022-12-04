cd6de899c07e9f57e86ffdad599977a2d0dbb311
Yoochanhong <chanhongy6@naver.com>
2022-12-04 21:50:46 +0900

    서버 생성 완료

---
+++ bin/server.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+// Configure routes.
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler)
+  ..get('/jsonData', _jsonHandler);
+
+Response _rootHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response _echoHandler(Request request) {
+  final message = request.params['message'];
+  return Response.ok('$message\n');
+}
+
+Response _jsonHandler(Request request) {
+  final jsonData = [
+    {"id": 1, "state": 0},
+    {"id": 2, "state": 0},
+    {"id": 3, "state": 0},
+    {"id": 4, "state": 1}
+  ];
+  return Response.ok(jsonEncode(jsonData).toString());
+}
+
+void main(List<String> args) async {
+  // Use any available host or container IP (usually `0.0.0.0`).
+  final ip = InternetAddress.anyIPv4;
+
+  // Configure a pipeline that logs requests.
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+
+  // For running in containers, we respect the PORT environment variable.
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
